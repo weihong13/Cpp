f259d479562a37a765494fa5b4f921c69d3ef52f
weihong13 <weihong010925@163.com>
2024-06-07 17:18:14 +0800

    517

---
+++ Cpp_517/test.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+using namespace std;
+
+//int main()
+//{
+//	string s1 = "abcdefgh";
+//	s1.push_back('x'); // Î²²å
+//	s1.pop_back(); // Î²É¾
+//	s1.push_back('x'); // Î²²å
+//
+//	// µü´úÆ÷
+//	for (string::iterator it = s1.begin(); it != s1.end(); it++)
+//	{
+//		cout << *it << endl;
+//
+//	}
+//
+//
+//	return 0;
+//}
+
+
+int main() {
+
+	int arr[5][5] = { 0 };
+
+
+
+	return 0;
+}
